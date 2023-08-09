8d0700bba7327f9d87f369404f72e7fe9f0c865e
John Mai <maiqingqiang@foxmail.com>
2023-08-09 17:08:59 +0800

    Fix "com.intellij.psi.stubs.ManyProjectsStubIndexMismatch: PSI and index do not match." #12

--- src/main/java/com/github/maiqingqiang/goormhelper/listeners/MyProjectManagerListener.java
+++ src/main/java/com/github/maiqingqiang/goormhelper/listeners/MyProjectManagerListener.java
@@ -3,6 +3,7 @@ package com.github.maiqingqiang.goormhelper.listeners;
 import com.github.maiqingqiang.goormhelper.services.GoORMHelperCacheManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManagerListener;
+import com.intellij.openapi.startup.StartupManager;
 import org.jetbrains.annotations.NotNull;
 
 public class MyProjectManagerListener implements ProjectManagerListener {
@@ -12,6 +13,6 @@ public class MyProjectManagerListener implements ProjectManagerListener {
     }
 
     private void intializing(@NotNull Project project) {
-        GoORMHelperCacheManager.getInstance(project).scan();
+        StartupManager.getInstance(project).runWhenProjectIsInitialized(() -> GoORMHelperCacheManager.getInstance(project).scan());
     }
 }
