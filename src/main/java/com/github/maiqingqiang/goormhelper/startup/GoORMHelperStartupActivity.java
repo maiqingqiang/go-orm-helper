48615d8d1821c6aa6ebf9c895d4086fb5062fe08
John Mai <maiqingqiang@foxmail.com>
2023-08-14 00:45:15 +0800

    :coffin: Remove Internal method.

--- src/main/java/com/github/maiqingqiang/goormhelper/startup/GoORMHelperStartupActivity.java
+++ src/main/java/com/github/maiqingqiang/goormhelper/startup/GoORMHelperStartupActivity.java
@@ -1,15 +1,15 @@
 package com.github.maiqingqiang.goormhelper.startup;
 
 import com.github.maiqingqiang.goormhelper.services.GoORMHelperCacheManager;
+import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
-import com.intellij.openapi.startup.StartupManager;
 import org.jetbrains.annotations.NotNull;
 
 public class GoORMHelperStartupActivity implements StartupActivity, DumbAware {
     @Override
     public void runActivity(@NotNull Project project) {
-        StartupManager.getInstance(project).runAfterOpened(() -> GoORMHelperCacheManager.getInstance(project).scan());
+        ReadAction.run(() -> GoORMHelperCacheManager.getInstance(project).scan());
     }
 }
